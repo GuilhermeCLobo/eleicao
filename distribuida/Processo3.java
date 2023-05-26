62076b5eddc8fd9cdb6ad673da5ab3ec277c2db7
Guilherme Crosera Lobo <74374381+GuilhermeCLobo@users.noreply.github.com>
2023-05-26 19:14:32 -0300

    Update Processo3.java

--- distribuida/Processo3.java
+++ distribuida/Processo3.java
@@ -4,7 +4,7 @@ public class Processo3 {
 	public Processo3() {
 		try {
 			ProcessosInterface m = new ProcessosImpl();
-            Registry registry = LocateRegistry.createRegistry(5002);
+            Registry registry = LocateRegistry.createRegistry(5000);
         	registry.rebind("Processo", m);
 			System.out.println("server ok");
 		}
